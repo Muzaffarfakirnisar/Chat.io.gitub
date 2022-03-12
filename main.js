568dca8bd96df0516710ce0c919bb7d7c994fab1
Muzaffarfakirnisar <98882949+Muzaffarfakirnisar@users.noreply.github.com>
2022-03-12 12:48:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+let http = require('http');
+let fs = require('fs');
+let cors = require('cors');
+const { Server } = require("socket.io");
+
+let port = process.env.PORT || 8080;
+server = http.createServer((req, res) => {
+  fs.readFile('index.html', 'utf-8', (er, html) => {
+    if (er) {
+      res.end('unabel to connect to server');
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  });
+
+
+});
+const io = new Server(server, {
+  cors: {
+    origin: "muzaffarfakirnisar.github.io/Chat.io.gitub/",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["*"],
+    credentials: true
+  }
+});
+let user = {};
+let on = 0;
+io.on('connection', (socket) => {
+  console.log('connection');
+  socket.on('send', (message) => {
+    socket.broadcast.emit('recive', { message: message, name: user[socket.id] });
+    console.log(`${message}`);
+  });
+});
+server.listen(port);
+io.attach(server)
